90474c98a09c92b2e42702ead6c6933b0ef2f66f
Bridgette Lemus <bridgettelemus@Bridgettes-MacBook-Pro.local>
2022-07-14 11:33:14 -0400

    labs on arrays and a couple labs on objects

---
+++ array.js
@@ -0,0 +1,37 @@
+
+//Create an array with a list of fruits starting with 3 fruit
+//with that array now do the following
+// - Add a new fruit to the end of the array
+// - Display the array
+// - Add a new fruit to the beginning of the array
+// - Display the array
+// - Remove the last fruit.
+// - Display the array
+
+// let fruits = ['kiwi', 'mango','apple']
+// fruits.push('avocado')
+// console.log('This is regular display', fruits)
+
+// // fruits.unshift('starfruit')
+// fruits.pop()
+// fruits.indexOf('Bridgette')
+// console.log(fruits.indexOf('Bridgette'))
+
+// console.log('This is regular display', fruits)
+// console.log('This is spread', ... fruits)
+
+const chickenEgg = ['🥚', '🐓'].sort()
+
+console.log(`The ${chickenEgg[0]} came first!`)
+
+
+
+
+
+
+
+
+
+
+
+
